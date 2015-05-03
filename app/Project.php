7ff7b43a7745935b22a38ae4995970b5f1d63c1f
Erica Terry <eterry@knights.ucf.edu>
2015-05-03 19:12:31 -0400

    Update to Project and Task classes to allow mass assignment

--- app/Project.php
+++ app/Project.php
@@ -4,8 +4,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model {
 
+    protected $guarded =[];
+
 	public function tasks()
-        {
+    {
             return $this->hasMany('App\Task');
-        }
+    }
 }
